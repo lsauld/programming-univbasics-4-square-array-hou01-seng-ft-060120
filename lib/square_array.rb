55af44451d954af42c14dc3311a73591c138aa08
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-05-17 02:11:41 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,23 @@
 def square_array(numbers)
-  new numarr = []
- counter = 0 
- while counter < numbers.length do
-   puts numbers[counter]
-   counter ** 2 
- end 
+  count = 0
+  new_num = []
   
-  new numarr 
+ while count < numbers.length do
+   new_num.push(numbers[count])
+   count ** 2   
+   
+ end 
+  new_num
+  
 end
   
   
-  
-  
+#def return_string_array(array)
+ # count = 0
+  #new_array = []
+  #while count < array.length do
+   # new_array.push(array[count].to_s) 
+   # count += 1
+  #end
+ # new_array
+#end
