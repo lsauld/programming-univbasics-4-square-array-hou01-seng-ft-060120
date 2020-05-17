97379decb0adc9574a6754ae0e3bb53ae5c3f416
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-05-17 02:35:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,21 +3,12 @@ def square_array(numbers)
   new_num = []
   
  while count < numbers.length do
-   new_num.push(numbers[count])
-   count ** 2   
+   new_num.push(numbers[count]count ** 2)   
+   count += 1
    
  end 
   new_num
   
 end
   
-  
-#def return_string_array(array)
- # count = 0
-  #new_array = []
-  #while count < array.length do
-   # new_array.push(array[count].to_s) 
-   # count += 1
-  #end
- # new_array
-#end
+ 
